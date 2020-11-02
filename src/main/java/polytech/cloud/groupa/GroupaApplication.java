1dd4262baa11cacbd066b7309f23b5a57f3820cb
EwaldJa <ewald.janin@users.noreply.github.com>
2020-11-02 11:59:52 +0100

    Initial commit Creating a Spring Boot project

---
+++ src/main/java/polytech/cloud/groupa/GroupaApplication.java
@@ -0,0 +1,13 @@
+package polytech.cloud.groupa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/polytech/cloud/groupa/GroupaApplicationTests.java
@@ -0,0 +1,13 @@
+package polytech.cloud.groupa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
