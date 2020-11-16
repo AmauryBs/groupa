61d4fc5686a4c20301eec6d7a47b431fe10987f6
EwaldJa <ewald.janin@users.noreply.github.com>
2020-11-16 11:58:16 +0100

    Corrected pom.xml should now starts

--- Dbgeneration.sql
+++ Dbgeneration.sql
@@ -1,9 +1,9 @@
 USE `bltnqtwwulzbsp3yfedg`;
 CREATE TABLE Users (
- `id` VARCHAR(255) NOT NULL,
- `firstName` VARCHAR(255),
- `lastName` VARCHAR(255),
- `birthDay` DATE,
- `lat` FLOAT,
- `long` FLOAT, 
- PRIMARY KEY (id));
+    `id` VARCHAR(255) NOT NULL,
+    `firstName` VARCHAR(255),
+    `lastName` VARCHAR(255),
+    `birthDay` DATE,
+    `lat` FLOAT,
+    `long` FLOAT,
+    PRIMARY KEY (id));
