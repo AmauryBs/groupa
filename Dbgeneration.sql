fdc58384e4ee037163a5964e87f1ed27473b8577
Amaury <43411870+AmauryBs@users.noreply.github.com>
2020-12-03 21:46:07 +0100

    rename positionId

--- Dbgeneration.sql
+++ Dbgeneration.sql
@@ -4,16 +4,16 @@ DROP TABLE IF EXISTS User;
 DROP TABLE IF EXISTS Position;
 
  CREATE TABLE Position (
-    `PositionID` int NOT NULL AUTO_INCREMENT,
+    `positionId` int NOT NULL AUTO_INCREMENT,
     `lat` FLOAT,
     `lon` FLOAT,
- PRIMARY KEY (PositionID));
+ PRIMARY KEY (positionId));
  
 CREATE TABLE User (
 	`id` int NOT NULL AUTO_INCREMENT,
     `firstName` VARCHAR(255),
     `lastName` VARCHAR(255),
     `birthDay` DATE,
-    `PositionID` int,
+    `positionId` int,
     PRIMARY KEY (id),
-    FOREIGN KEY (PositionID) REFERENCES Position (PositionID));
+    FOREIGN KEY (positionId) REFERENCES Position (positionId));
